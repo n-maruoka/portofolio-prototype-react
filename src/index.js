04303cfb35889a34c7b2a1ca584ce644e3567f86
n-maruoka <nobuaki.maruoka@gmail.com>
2020-11-01 16:32:36 +0900

    clean App.js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,15 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as reducers from './reducers';
-// import reportWebVitals from './reportWebVitals';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// create Store
+const store = createStore(
+  combineReducers(reducers)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
